f20a325f97ffee38d84e41f7e36faa6c9d15e249
dgisolfi <daniel.gisolfi1@marist.edu>
2017-11-30 02:13:14 -0500

    lost and found location is now set by a combobox

--- limboPages/reportfound.php
+++ limboPages/reportfound.php
@@ -52,7 +52,46 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') {
 					<p>Submit records of found items within the marist campus.</p>
 					<form action="reportfound.php">
 						<br>Location:<br>
-					  	<input id="text" name="location" value="">
+					  	<select id="text" name="location" >
+     						<option value="">Select Building</option>
+						    <option value="--Any--">--Any--</option>
+					     	<option value="1">Allied Health Science Building</option>
+				     		<option value="2">Byrne House</option>
+				     		<option value="3">Cannavino Library </option>
+				     		<option value="4">Champagnat Hall</option>
+				     		<option value="5">Chapel</option>
+				     		<option value="6">Cornell Boathouse </option>
+				     		<option value="7"> Donnelly Hall</option>
+				     		<option value="8">Dyson Center</option>
+				     		<option value="9">Fern Tor</option>
+				     		<option value="10">Fontaine Hall</option>
+				     		<option value="11">Foy Townhouses</option>
+				     		<option value="12">Greystone Hall</option>
+				     		<option value="13">Hancock Center</option>
+				     		<option value="14">Kieran Gatehouse</option>
+				     		<option value="15">Kirk House</option>
+				     		<option value="16">Lower Fulton Townhouses </option>
+				     		<option value="17">Leo Hall</option>
+				     		<option value="18">Longview Park </option>
+				     		<option value="19">Lowell Thomas Communications Center</option>
+				     		<option value="20">Lower New Townhouses</option>
+				     		<option value="21">Lower West Townhouses</option>
+				     		<option value="22">Marian Hall</option>
+				     		<option value="23">Marist Boathouse</option>
+				     		<option value="24">McCann Center</option>
+				     		<option value="25">Mid-Rise Hall</option>
+				     		<option value="26">New Gartland A</option>
+				     		<option value="27">New Gartland B</option>
+				     		<option value="28">New Gartland C</option>
+				     		<option value="29">New Gartland D</option>
+				     		<option value="30">St. Ann's Hermitage</option>
+				     		<option value="31">St. Peter's</option>
+				     		<option value="32">Sheahan Hall</option>
+				     		<option value="33">Steel Plant Studios</option>
+				     		<option value="34">Student Center</option>
+				     		<option value="35">Upper Fulton Townhouses</option>
+				     		<option value="36">Upper West Townhouses </option>
+  						</select>
 					  	<br>Item Name:<br>
 					  	<input id="text" name="name" value="">
 						<br>Description:<br>
@@ -75,4 +114,9 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') {
 		 </div>
 	</body>
 </html>
+
+
+
+					
+
 
